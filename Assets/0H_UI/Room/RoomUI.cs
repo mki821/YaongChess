91a4534709ba26ea61489dabd868bac08c2b0077
mki821 <alexalex0821@naver.com>
2023-11-13 00:50:51 +0900

    서버 버그 수정중...

--- Assets/0H_UI/Room/RoomUI.cs
+++ Assets/0H_UI/Room/RoomUI.cs
@@ -1,8 +1,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 
-class RoomInfo {
+public class RoomInfo {
     public int roomID;
     public string roomName;
     public int personnel;
@@ -10,20 +11,62 @@ class RoomInfo {
 
 public class RoomUI : MonoBehaviour
 {
+    [SerializeField] private VisualTreeAsset _roomInfoTemplate;
+    [SerializeField] private VisualTreeAsset _loadingPanelTemplate;
+
     private UIDocument _uiDocument;
     private VisualElement _roomList;
+    private VisualElement _loadingPanel;
 
     private void Awake() {
         _uiDocument = GetComponent<UIDocument>();
     }
 
     private void Start() {
+        TCPClient.EventListener["refresh.room"] = RefreshRoom;
+        TCPClient.EventListener["try.room"] = ConnectRoom;
+
         var root = _uiDocument.rootVisualElement;
 
         _roomList = root.Q<VisualElement>("room-list");
+        root.Q<VisualElement>("refresh").RegisterCallback<ClickEvent>(e => TCPClient.SendBuffer("room.refresh", null));
     }
 
-    //public void RefreshRoom(Dictionary<int, RoomInfo> roomInfos) {
+    public void RefreshRoom(LitJson.JsonData jsondata) {
+        var roomInfos = LitJson.JsonMapper.ToObject<RoomInfo[]>((string)jsondata);
 
-    //}
+        foreach(RoomInfo item in roomInfos) {
+            var template = _roomInfoTemplate.Instantiate().Q<VisualElement>("room");
+
+            template.Q<Label>("room-name").text = item.roomName;
+            template.Q<Label>("room-host").text = item.roomID.ToString();
+            template.Q<Label>("room-personnel").text = $"{item.personnel} / 2";
+
+            template.RegisterCallback<ClickEvent>(e =>TryConnectRoom(item.roomID));
+
+            _roomList.Add(template);
+        }
+    }
+
+    private void MakeRoom() {
+        //
+    }
+
+    private void TryConnectRoom(int roomID) {
+        TCPClient.SendBuffer("room.connect", roomID);
+
+        if(_loadingPanel == null)
+            _loadingPanel = _loadingPanelTemplate.Instantiate().Q<VisualElement>("loading-panel");
+    }
+
+    private void ConnectRoom(LitJson.JsonData jsondata) {
+        bool success = LitJson.JsonMapper.ToObject<bool>(jsondata.ToJson());
+
+        if(success) {
+            SceneManager.LoadScene(2);
+        }
+        else {
+            _loadingPanel.Clear();
+        }
+    }
 }
